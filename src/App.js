6ba6e9f6ed329e170377a4b49661aaa5eb2aeeab
catjeed <catjeed@yandex.ru>
2023-11-25 19:05:33 +0500

    create html page react-pizza

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import Card from './components/Card';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page">
+        <div className="content">
+          <div className="header">
+            <div className="headerTitle">
+              <img src="/images/logo.svg" alt="лого" className="logo" />
+              <div>
+                <h2 className="title">React Pizza</h2>
+                <p className="subtitle">самая вкусная пицца во вселенной</p>
+              </div>
+            </div>
+            <div>
+              <ul className="cartButton">
+                <li className="cartButtonUnit">520 &#8381;</li>
+                <span className="lineBetween"></span>
+                <li className="cartButtonUnit">
+                  <img src="/images/cart.svg" alt="Корзина" />
+                  <p>3</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="pizzaSort">
+            <ul className="pizzaTypes">
+              <li className="pizzaType">Все</li>
+              <li className="pizzaType">Мясные</li>
+              <li className="pizzaType">Вегетарианская</li>
+              <li className="pizzaType">Гриль</li>
+              <li className="pizzaType">Острые</li>
+              <li className="pizzaType">Закрытые</li>
+            </ul>
+            <div className="sort">
+              <img src="/images/triangle.svg" alt="стрелочка" />
+              <ul className="sortList">
+                <p>Сортировка по:</p>
+                <li className="sortListUnit">популярности</li>
+              </ul>
+            </div>
+          </div>
+          <h1 className="mainTitle">Все пиццы</h1>
+          <div className="main">
+            <div className="cards">
+              <Card name={'Чизбургер-пицца'} imageUrl={'./images/pizza.png'} />
+              <Card name={'Сырная'} imageUrl={'./images/cheese.png'} />
+              <Card
+                name={'Креветки по-азиатски'}
+                imageUrl={'./images/krevedko.png'}
+              />
+              <Card
+                name={'Сырный цыпленок'}
+                imageUrl={'./images/cheesyChicken.png'}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Card({ name, imageUrl }) {
+  return (
+    <div className="cardItem">
+      <img src={imageUrl} alt={name} className="cardItemImage" />
+      <h3 className="cardItemName">{name}</h3>
+      <div className="cardItemProps">
+        <ul className="cardItemProp">
+          <li className="cardItemPropType doughType">Тонкое</li>
+          <li className="cardItemPropType doughType">Традиционное</li>
+        </ul>
+        <ul className="cardItemProp">
+          <li className="cardItemPropType doughSize">26 см.</li>
+          <li className="cardItemPropType doughSize">30 см.</li>
+          <li className="cardItemPropType doughSize outOfStock">40 см.</li>
+        </ul>
+      </div>
+      <div className="cardItemPriceAndBuy">
+        <p className="cardItemPrice"> от 395 &#8381;</p>
+        <button className="cardItemBuyButton">Добавить</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
